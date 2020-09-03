bd1bf128c38c61261373a48a48e909e8455e09c4
George F <64555318+GAFelton@users.noreply.github.com>
2020-09-03 10:47:09 -0700

    Basic schema for employee_DB

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE employee_DB;
+
+USE employee_DB;
+
+CREATE TABLE department(
+  department_id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY (department_id)
+);
+
+CREATE TABLE role(
+  role_id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department_id INT,
+   FOREIGN KEY [department_id](department_id)
+    REFERENCES department(department_id),
+  PRIMARY KEY (role_id)
+);
+
+CREATE TABLE employees(
+  employee_id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY [role_id](role_id)
+    REFERENCES role(role_id),
+  FOREIGN KEY [manager_id](employee_id)
+    REFERENCES employee(employee_id),
+  PRIMARY KEY (employee_id)
+);
+
+
