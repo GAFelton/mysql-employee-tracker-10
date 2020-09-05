97a1e928f923f3ba2ab202cc7a1647700edc4da8
George F <64555318+GAFelton@users.noreply.github.com>
2020-09-04 19:07:49 -0700

    Added mainPrompt switch logic, and view department/role/employee async functions.

--- app.js
+++ app.js
@@ -6,8 +6,17 @@ const prompts = require('./prompts');
 const db = require('./db');
 require('console.table');
 
-async function viewallRoles() {
-    const roles = await db.viewallRoles();
+async function viewAllDepts() {
+    const depts = await db.viewAllDepts();
+
+    console.log('\n');
+    console.table(depts);
+
+    mainPrompt();
+}
+
+async function viewAllRoles() {
+    const roles = await db.viewAllRoles();
 
     console.log('\n');
     console.table(roles);
@@ -15,11 +24,48 @@ async function viewallRoles() {
     mainPrompt();
 }
 
+async function viewAllEmployees() {
+    const employees = await db.viewAllEmployees();
+
+    console.log('\n');
+    console.table(employees);
+
+    mainPrompt();
+}
+
+async function addEmployee() {
+    const { firstName, lastName, roleID, managerID } = await inquirer.prompt(prompts.addEmployee);
+    console.log(firstName, lastName, roleID, managerID);
+    // const newEmployee = await db.addEmployee();
+
+    // console.log('\n');
+    // console.table(newEmployee);
+
+    mainPrompt();
+}
+
 
 async function mainPrompt() {
-    const { choice } = await inquirer.prompt(prompts.mainPrompt);
+    const { mainPrompt } = await inquirer.prompt(prompts.mainPrompt);
 
-    switch (choice) {
-
+    switch (mainPrompt) {
+        case "View All Departments":
+            viewAllDepts();
+            break;
+        case "View All Roles":
+            viewAllRoles();
+            break;
+        case "View All Employees":
+            viewAllEmployees();
+            break;
+        case "Add New Employee":
+            addEmployee();
+            break;
+        case "exit":
+            connection.end();
+            break;
     }
+    
 }
+
+mainPrompt();
