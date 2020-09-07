17789b1b935751916be013b1bbd3ec3413986feb
George F <64555318+GAFelton@users.noreply.github.com>
2020-09-06 22:36:06 -0700

    Cleaned up employees in seeds.sql.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,13 @@
 USE employee_db;
 
 INSERT INTO departments (name)
-VALUES ("engineering"), ("product"), ("design"), ("sales"), ("marketing");
+VALUES 
+        ("engineering"), ("product"), ("design"), ("sales"), ("marketing");
 
 
 INSERT INTO roles (title, salary, department_id)
-VALUES ("Software Engineer", 60000, 1),
+VALUES 
+        ("Software Engineer", 60000, 1),
         ("Web Developer", 55000, 1),
         ("Technical Lead", 70000, 1),
         ("Product Manager", 70000, 2),
@@ -16,16 +18,18 @@ VALUES ("Software Engineer", 60000, 1),
         ("Product Marketing Manager", 55000, 5)
         ;
 
+SET FOREIGN_KEY_CHECKS=0;
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES 
-        ("Roger", "Barton", 3, null),
-        ("Yousef", "Rubenstein", 4, null),
-        ("Ambros", "Pantopolos", 7, null),
-        ("Burt", "Rayman", 8, null),
-        ("Angelina", "Cruz", 9, null),
-        ("Joe", "Smith", 1, 1),
-        ("Betty", "Comstock", 2, 1),
-        ("Amy", "Cortez", 2, 1),
+        ("Yousef", "Rubenstein", 4, 1),
+        ("Roger", "Barton", 3, 1),
+        ("Joe", "Smith", 1, 2),
+        ("Ambros", "Pantopolos", 7, 1),
+        ("Burt", "Rayman", 8, 1),
+        ("Angelina", "Cruz", 9, 1),
+        ("Betty", "Comstock", 2, 2),
+        ("Amy", "Cortez", 2, 2),
         ("Carly", "Rupert", 5, 2),
-        ("Genevieve", "LaMont", 6, 3)
+        ("Genevieve", "LaMont", 6, 4)
 ;
+SET FOREIGN_KEY_CHECKS=1;
